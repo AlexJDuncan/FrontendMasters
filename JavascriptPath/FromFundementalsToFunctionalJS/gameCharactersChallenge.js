432f756f9f569e5252c8da1d68e0c7d2b2d350a2
alex <monkeymedic26@gmail.com>
2021-10-16 19:11:39 -0700

    misunderstood challenge.  solution modified

--- JavascriptPath/FromFundementalsToFunctionalJS/gameCharactersChallenge.js
+++ JavascriptPath/FromFundementalsToFunctionalJS/gameCharactersChallenge.js
@@ -1,6 +1,28 @@
-let pieces = {
-    characters = ["Mrs. White", "Mr. Green", "Col Mustard", "Ms. Peacock", "Ms. Scarlet", "Prof Plum"],
-    weapons = ["candlestick", "rope", "revolver", "lead pipe", "knife"],
-    rooms = ["Billiard Room", "Conservatory", "Study", "Library", "Kitchen", "Dining Room"],
-    confidential = []
-}
+var game = {};
+
+game.characters = [];
+game.weapons = [];
+game.rooms = [];
+game.confidential = [];
+
+game.characters[0] = "Mrs. White";
+game.characters[1] = "Mr. Green";
+game.characters[2] = "Ms. Peacock";
+game.characters[3] = "Ms. Scarlet";
+game.characters[4] = "Col Mustard";
+game.characters[5] = "Prof Plum";
+
+game.weapons[0] = "rope";
+game.weapons[1] = "lead pipe";
+game.weapons[2] = "revolver";
+game.weapons[3] = "candlestick";
+game.weapons[4] = "knife";
+
+game.rooms[0] = "Billiard Room";
+game.rooms[1] = "Conservatory";
+game.rooms[2] = "Kitchen";
+game.rooms[3] = "Study";
+game.rooms[4] = "Dining Room";
+game.rooms[5] = "Library";
+
+console.log(game);
