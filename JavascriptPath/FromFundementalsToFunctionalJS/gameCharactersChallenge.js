89da1ac31a1820b865efccdcf08330b3b0739d18
alex <monkeymedic26@gmail.com>
2021-10-16 19:03:47 -0700

    my solution

---
+++ JavascriptPath/FromFundementalsToFunctionalJS/gameCharactersChallenge.js
@@ -0,0 +1,6 @@
+let pieces = {
+    characters = ["Mrs. White", "Mr. Green", "Col Mustard", "Ms. Peacock", "Ms. Scarlet", "Prof Plum"],
+    weapons = ["candlestick", "rope", "revolver", "lead pipe", "knife"],
+    rooms = ["Billiard Room", "Conservatory", "Study", "Library", "Kitchen", "Dining Room"],
+    confidential = []
+}
