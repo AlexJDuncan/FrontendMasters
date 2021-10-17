8c22a2cee581db9d12861cf9414e58088c7e76ce
alex <monkeymedic26@gmail.com>
2021-10-17 14:36:51 -0700

    partial answer from the vid

--- JavascriptPath/FromFundementalsToFunctionalJS/gameCharactersChallenge.js
+++ JavascriptPath/FromFundementalsToFunctionalJS/gameCharactersChallenge.js
@@ -1,7 +1,8 @@
+// my solution
+
 var game = {};
 
 game.characters = [];
-game.weapons = [];
 game.rooms = [];
 game.confidential = [];
 
@@ -12,10 +13,10 @@ game.characters[3] = "Ms. Scarlet";
 game.characters[4] = "Col Mustard";
 game.characters[5] = "Prof Plum";
 
-game.weapons[0] = "rope";
-game.weapons[1] = "lead pipe";
-game.weapons[2] = "revolver";
-game.weapons[3] = "candlestick";
+game.weapons[0] = "rope"
+game.weapons[1] = "lead pipe"
+game.weapons[2] = "revolver"
+game.weapons[3] = "candlestick"
 game.weapons[4] = "knife";
 
 game.rooms[0] = "Billiard Room";
@@ -26,3 +27,6 @@ game.rooms[4] = "Dining Room";
 game.rooms[5] = "Library";
 
 console.log(game);
+
+// video solution with student assistance
+
