9783c65f719627fa90ecee7e43c0688ea82abac0
monkeymedic26 <monkeymedic26@gmail.com>
2021-10-20 13:33:30 -0700

    added a breed select

--- ReactPath/CompleteIntroToReact/adoptMe/src/SearchParams.js
+++ ReactPath/CompleteIntroToReact/adoptMe/src/SearchParams.js
@@ -5,6 +5,9 @@ const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
   const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
+
+  const breeds = [];
 
   function updateLocation(e) {
     setLocation(e.target.value);
@@ -26,7 +29,8 @@ const SearchParams = () => {
           <select
             id="animal"
             value={animal}
-            onChange={e => setAnimal(e.target.value)}
+            onChange={(e) => setAnimal(e.target.value)}
+            onBlur={(e) => setAnimal(e.target.value)}
           >
             <option />
             {
@@ -34,6 +38,26 @@ const SearchParams = () => {
                 return (
                   <option value={animal} key={animal}>
                     {animal}
+                  </option>
+                )
+              })
+            }
+          </select>
+          </label>
+          <label htmlFor="breed">
+          Breed
+          <select
+            id="breed"
+            value={breed}
+            onChange={(e) => setBreed(e.target.value)}
+            onBlur={(e) => setBreed(e.target.value)}
+          >
+            <option />
+            {
+              breeds.map(breed => {
+                return (
+                  <option value={breed} key={breed}>
+                    {breed}
                   </option>
                 )
               })
