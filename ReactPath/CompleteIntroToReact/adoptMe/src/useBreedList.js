b27f1959eaca4451ee76873aec37f9d19de0cda1
monkeymedic26 <monkeymedic26@gmail.com>
2021-10-20 21:04:50 -0700

    fixed bug in useBreedList and added to SearchParams

--- ReactPath/CompleteIntroToReact/adoptMe/src/useBreedList.js
+++ ReactPath/CompleteIntroToReact/adoptMe/src/useBreedList.js
@@ -8,8 +8,8 @@ export default function useBreedList(animal) {
 
   useEffect(() => {
     if (!animal) {
-      setBreedList([])
-    } else if (localCacke[animal]) {
+      setBreedList([]);
+    } else if (localCache[animal]) {
       setBreedList(localCache[animal])
     } else {
       requestBreedList();
@@ -20,7 +20,7 @@ export default function useBreedList(animal) {
 
       const res = await fetch(
         `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
-      )
+      );
       const json = await res.json();
       localCache[animal] = json.breeds || [];
       setBreedList(localCache[animal]);
