b61fe3dddd5b8fb1a0b9f3e817c2ac3ad4180d70
monkeymedic26 <monkeymedic26@gmail.com>
2021-10-22 13:59:17 -0700

    challenge completed

---
+++ JavascriptPath/FromFundementalsToFunctionalJS/destructuringChallenge.js
@@ -0,0 +1,13 @@
+// create an object that looks like this
+// {"name": "Rusty", "room": "kitchen", "weapon": "candlestick"}
+// extract out the weapon and location using destructuring
+
+const obj = {
+  name: "Rusty",
+  room: "kitchen",
+  weapon: "candlestick"
+}
+
+const { name, room, weapon } = {name: "Rusty", room: "kitchen", weapon: "candlestick"};
+
+
