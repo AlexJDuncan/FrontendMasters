605f33d6db8c15ac51f2a98e224eefe9479a1698
monkeymedic26 <monkeymedic26@gmail.com>
2021-10-22 14:17:09 -0700

    added some examples of destructuring

--- JavascriptPath/FromFundementalsToFunctionalJS/destructuringChallenge.js
+++ JavascriptPath/FromFundementalsToFunctionalJS/destructuringChallenge.js
@@ -10,4 +10,37 @@ const obj = {
 
 const { name, room, weapon } = {name: "Rusty", room: "kitchen", weapon: "candlestick"};
 
+// omit certain values
+var [a, , b] = [1, 2, 3];
+console.log(a, b);
+// => 1 3
+
+// combine with spread/rest operator (accumulates the rest of the values)
+var [a, ...b] = [1, 2, 3];
+console.log(a, b);
+// => 1 [2, 3]
+
+// Swap variables easily without temp
+var a = 1, b = 2;
+[b, a] = [a, b];
+console.log(a, b);
+// => 2 1
+
+// advance deep arrays
+var [a, [b, [c, d]]] = [1, [2, [[[3, 4], 5], 6]]];
+console.log("a:", a, "b:", b, "c:", c, "d:", d);
+// => a: 1 b: 2 c: [[3,4], 5] d: 6
+
+// === objects
+
+var {user: x} = {user: 5};
+console.log(x);
+// => 5
+
+// Fail-safe
+
+var {user: x} = {user2: 5};
+console.log(x);
+// => undefined
+
 
