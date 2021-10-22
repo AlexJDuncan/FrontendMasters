01e25c61e65f4b19d96f88d98d7b70de75d549a3
monkeymedic26 <monkeymedic26@gmail.com>
2021-10-22 14:34:18 -0700

    added my solution and video solution

---
+++ JavascriptPath/FromFundementalsToFunctionalJS/loopingExercise.js
@@ -0,0 +1,36 @@
+//loop through the suspects array
+
+const game = {
+  'suspects': [
+    {
+      name: "Rusty",
+      color: "orange"
+    },
+    {
+      name: "Miss Scarlet",
+      color: "red"
+    }
+  ]
+}
+
+const loopObj = (game) => {
+  const name = [];
+  const color = [];
+
+  for (let i = 0; i < game['suspects'].length; i++) {  //could do game.suspects.length instead
+    name.push(game['suspects'][i].name);
+    color.push(game['suspects'][i].color);
+  }
+  return name && color;
+}
+
+loopObj(game);
+
+//video solution
+function foo(game) {
+  for (let i = 0; i < game.suspects.length; i++) {
+    console.log(game.suspects[i]);
+  }
+}
+
+foo(game);
