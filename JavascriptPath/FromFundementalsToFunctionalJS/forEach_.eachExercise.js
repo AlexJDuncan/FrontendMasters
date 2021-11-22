583339a570a1910206a14528fdd22b2eff776283
monkeymedic26 <monkeymedic26@gmail.com>
2021-11-22 14:30:07 -0800

    added video solution

--- JavascriptPath/FromFundementalsToFunctionalJS/forEach_.eachExercise.js
+++ JavascriptPath/FromFundementalsToFunctionalJS/forEach_.eachExercise.js
@@ -12,22 +12,30 @@
 // Exercise
 // Complete the rest of this function so that it works as described in the previous slides:
  _.each = function(list, callback) {
-   // loop through array
+   // loop through list
    if (Array.isArray(list)) {
      // call the callback with a list item
      for (var i = 0; i < list.length; i++) {
-       callback(list[i], i, list);
+       callback(list[i]);
      }
-   } else {
-     // loop through object
-    for (var key in list) {
-      // call the callback with a list item
-      callback(list[key], key, list);
-    }
-  }
+   }
+
+   if (typeof(list) === object) {
+     for (var key in list) {
+       callback(list[key]);
+     }
+   }
 }
 
 // Solution
 _.each = function(list, callback) {
-
+  if (Array.isArray(list)) {
+    for (var i = 0; i < list.length; i++) {
+      callback(list[i], i, list);
+    }
+  } else {
+    for (var key in list) {
+      callback(list[key], key, list);
+    }
+  }
 }
