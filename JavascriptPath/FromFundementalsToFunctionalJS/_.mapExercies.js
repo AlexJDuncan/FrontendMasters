a0c2bde78fee66ffa4db63f7b0f9fb4de20d5d78
monkeymedic26 <monkeymedic26@gmail.com>
2021-11-22 14:51:27 -0800

    refactored for both array and object inputs

--- JavascriptPath/FromFundementalsToFunctionalJS/_.mapExercies.js
+++ JavascriptPath/FromFundementalsToFunctionalJS/_.mapExercies.js
@@ -12,8 +12,14 @@ let brokenWeapons = _.map(weapons, makeBroken);
 // rewrite _.map
 _.map = function(list, callback) {
   let result = [];
-  for (var i = 0; i < list.length; i++) {
-    result.push(callback(list[i]));
+  if (Array.isArray(list)) {
+    for (var i = 0; i < list.length; i++) {
+      result.push(callback(list[i], i, list));
+    }
+  } else {
+    for (var key in list) {
+      result.push(callback(list[key], key, list));
+    }
   }
   return result;
 }
