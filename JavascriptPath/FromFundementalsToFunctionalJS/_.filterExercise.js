eab4b23be97ffa4cc3ed39eaf87ccf2190fc1a7c
monkeymedic26 <monkeymedic26@gmail.com>
2021-11-24 15:56:50 -0800

    added a version using each

--- JavascriptPath/FromFundementalsToFunctionalJS/_.filterExercise.js
+++ JavascriptPath/FromFundementalsToFunctionalJS/_.filterExercise.js
@@ -2,17 +2,31 @@
 
 const _ = {};
 
+// _.filter = function(arr, cb) {
+//   // create a new array
+//   const storage =[];
+//   //loop through arr
+//   for (let i = 0; i = arr.length; i++) {
+//     //check if cb returns true
+//     if (cb(list[i], i, arr) === true) {
+//       //if return true push into array
+//       storage.push(arr[i]);
+//     }
+//   }
+//   // return array
+//   return storage;
+// }
+
+// With _.each
+
 _.filter = function(arr, cb) {
-  // create a new array
-  const storage =[];
-  //loop through arr
-  for (let i = 0; i = arr.length; i++) {
-    //check if cb returns true
-    if (cb(list[i], i, arr) === true) {
-      //if return true push into array
-      storage.push(arr[i]);
+  const storage = [];
+
+  _.each(arr, function(val, i, list) {
+    if (cb(val, i, list) === true) {
+      storage.push(val);
     }
-  }
-  // return array
+  });
+
   return storage;
 }
